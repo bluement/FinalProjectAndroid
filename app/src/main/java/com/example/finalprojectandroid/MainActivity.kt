0c0dea57c10e328e4d7ba9e5f1bcad73b1cf8bd6
Ricardo Falcao <ricardolfalcao.2005@gmail.com>
2024-04-30 13:26:37 -0400

    tdee calculator popozao no chao

--- app/src/main/java/com/example/finalprojectandroid/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectandroid/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnBmiCalculator).setOnClickListener {
-            startActivity(Intent(this, BmiCalculatorActivity::class.java))
+            startActivity(Intent(this, BmiCalculator::class.java))
         }
 
         findViewById<Button>(R.id.btnTdeeCalculator).setOnClickListener {
